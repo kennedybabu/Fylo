e9d4d5e9784d1e68a2a42f651eeef798594e4f68
kennedybabu <kennedynjogu8@gmail.com>
2022-06-03 09:55:12 +0300

    feature: create and style the navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Logo from '../assets/logo.svg'
+import {GoThreeBars} from 'react-icons/go'
+import {FaTimes} from 'react-icons/fa'
+
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => setNav(!nav)
+
+  return (
+    <div className='fixed top-0 left-0 right-0 w-full h-[80px] bg-sky-900 flex justify-between px-8 items-center text-white'>
+        <div>
+            <img src={Logo} alt="logo" className='w-20'/>
+        </div>
+
+        {/* navlinks */}
+        <div className='hidden md:flex text-xl'>
+            <li>Features</li>
+            <li>Team</li>
+            <li>Sign In</li>
+        </div>
+
+        {/* mobile-links */}
+        <div className={!nav ? 'hidden' : 'absolute top-0 left-0 right-0 h-screen w-full flex flex-col items-center justify-center font-bold bg-sky-900 duration-300'}>
+            <li>Features</li>
+            <li>Team</li>
+            <li>Sign In</li>
+        </div>
+
+        {/* nav toggler */}
+        <div onClick={handleNav} className='md:hidden z-20'>            
+            {!nav ? <GoThreeBars /> : <FaTimes />}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

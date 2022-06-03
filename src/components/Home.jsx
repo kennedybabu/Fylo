e89d376400ce69b4e00f3b1dd290e37911b24233
kennedybabu <kennedynjogu8@gmail.com>
2022-06-03 10:24:09 +0300

    feature: style the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,15 +3,16 @@ import LandingIllustration from '../assets/illustration-intro.png'
 
 const Home = () => {
   return (
-    <div className='bg-[#1c2431] h-screen flex flex-col px-8 items-center justify-center text-white'>
+    <div className='bg-[#1c2431] h-auto flex flex-col px-8 items-center justify-center text-white'>
         {/* container */}
-        <div>
+        <div className='mt-20'>
             <img src={LandingIllustration} alt="landing picture" />
         </div>
         <div className='text-center'>
-            <h2 className='text-3xl font-bold my-4 max-w-[600px] mx-auto'>All your files in one secure location, accessible anywhere.</h2>
-            <p>Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
+            <h2 className='text-4xl font-bold my-5 max-w-[600px] mx-auto'>All your files in one secure location, accessible anywhere.</h2>
+            <p className='text-xl max-w-[500px] mx-auto py-4'>Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
         </div>
+        <button className='py-2 px-14 rounded-3xl my-2 bg-gradient-to-r from-cyan-500 to-blue-500'>Get Started</button>
 
     </div>
   )
